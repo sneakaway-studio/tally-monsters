8b968519b2de9899df4de207d40d989eba1daea6
Owen Mundy <omundy@gmail.com>
2017-10-23 08:17:37 -0400

    Moves monster data into monster-data.js

---
+++ monster-data.js
@@ -0,0 +1,110 @@
+
+// DO NOT EDIT: 
+// ... copy and paste here from Dropbox > Product Monsters > monsters-master.js
+
+var monster_data = 
+	[
+		{
+			"name": "appliances-blender",
+			"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+			"taxonomy": "FROM THE TAXONOMY LIST",
+			"tags": ["appliance", "blender"],
+			"height": 400, "width": 1500,
+		},
+		{
+			"name": "car-suv-luxury",
+			"title": "Luxury SUV",
+			"taxonomy": "automotive and vehicles^cars^luxury",
+			"tags": ["car", "suv", "luxury"],
+			"height": 400, "width": 1500,
+		},
+		{
+			"name": "finance-bank-atm",
+			"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+			"taxonomy": "FROM THE TAXONOMY LIST",
+			"tags": ["finance", "bank", "atm"],
+			"height": 400, "width": 1500,
+		},
+		{
+			"name": "game-controller",
+			"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+			"taxonomy": "FROM THE TAXONOMY LIST",
+			"tags": ["game", "controller"],
+			"height": 400, "width": 1500,
+		},
+		{
+			"name": "gas-can",
+			"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+			"taxonomy": "FROM THE TAXONOMY LIST",
+			"tags": ["gas", "can"],
+			"height": 400, "width": 2500,
+		},
+		{
+			"name": "health-fit-watch",
+			"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+			"taxonomy": "FROM THE TAXONOMY LIST",
+			"tags": ["health", "fit", "watch"],
+			"height": 400, "width": 1500,
+		},
+		{
+			"name": "home-furniture-chest",
+			"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+			"taxonomy": "FROM THE TAXONOMY LIST",
+			"tags": ["home", "furniture"],
+			"height": 400, "width": 1500,
+		},
+		{
+			"name": "home-house",
+			"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+			"taxonomy": "FROM THE TAXONOMY LIST",
+			"tags": ["home", "house"],
+			"height": 400, "width": 1500,
+		},
+		{
+			"name": "home-plug",
+			"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+			"taxonomy": "FROM THE TAXONOMY LIST",
+			"tags": ["home", "plug"],
+			"height": 400, "width": 1500,
+		},
+		{
+			"name": "home-tables-stacked",
+			"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+			"taxonomy": "FROM THE TAXONOMY LIST",
+			"tags": ["home", "tables"],
+			"height": 400, "width": 1500,
+		},
+		//	{ 
+		//	"name":"hoverboard", "title":"A TITLE IN SENTENCE CASE FOR AWARDS", "taxonomy":"FROM THE TAXONOMY LIST", 
+		//	"tags": ["electronics","hoverboard"], "height":400, "width":1500,  },
+		//	{ 
+		//	"name":"lady-lamp", "title":"A TITLE IN SENTENCE CASE FOR AWARDS", "taxonomy":"FROM THE TAXONOMY LIST", 
+		//	"tags": ["electronics","lamp"], "height":400, "width":2500,  },
+		//	{ 
+		//	"name":"purse", "title":"A TITLE IN SENTENCE CASE FOR AWARDS", "taxonomy":"FROM THE TAXONOMY LIST", 
+		//	"tags": ["apparal","purse"], "height":400, "width":1500, },
+		{
+			"name": "shopping-cart",
+			"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+			"taxonomy": "FROM THE TAXONOMY LIST",
+			"tags": ["shopping", "cart"],
+			"height": 400, "width": 1500,
+		},
+		{
+			"name": "surveillance-camera",
+			"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+			"taxonomy": "FROM THE TAXONOMY LIST",
+			"tags": ["electronics", "surveillance", "camera"],
+			"height": 400, "width": 1500,
+		},
+		{
+			"name": "toy-sit-spin",
+			"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+			"taxonomy": "FROM THE TAXONOMY LIST",
+			"tags": ["toy", "spin"],
+			"height": 400, "width": 1500,
+		},
+		//	{ 
+		//	"name":"vr-fish", "title":"A TITLE IN SENTENCE CASE FOR AWARDS", "taxonomy":"FROM THE TAXONOMY LIST", 
+		//	"tags": ["electronics","vr"], "height":400, "width":1500, },
+	]
