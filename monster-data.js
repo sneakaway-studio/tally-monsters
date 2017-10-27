869a6c126a0a1495e3e9a8c0a3885c1a2ec833c3
Owen Mundy <omundy@gmail.com>
2017-10-27 08:19:52 -0400

    Adds gitignore, updates monster-data to key:obj, adds new monsters

--- monster-data.js
+++ monster-data.js
@@ -2,109 +2,144 @@
 // DO NOT EDIT: 
 // ... copy and paste here from Dropbox > Product Monsters > monsters-master.js
 
-var monster_data = 
-	[
-		{
-			"name": "appliances-blender",
-			"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
-			"taxonomy": "FROM THE TAXONOMY LIST",
-			"tags": ["appliance", "blender"],
-			"height": 400, "width": 1500,
-		},
-		{
-			"name": "car-suv-luxury",
-			"title": "Luxury SUV",
-			"taxonomy": "automotive and vehicles^cars^luxury",
-			"tags": ["car", "suv", "luxury"],
-			"height": 400, "width": 1500,
-		},
-		{
-			"name": "finance-bank-atm",
-			"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
-			"taxonomy": "FROM THE TAXONOMY LIST",
-			"tags": ["finance", "bank", "atm"],
-			"height": 400, "width": 1500,
-		},
-		{
-			"name": "game-controller",
-			"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
-			"taxonomy": "FROM THE TAXONOMY LIST",
-			"tags": ["game", "controller"],
-			"height": 400, "width": 1500,
-		},
-		{
-			"name": "gas-can",
-			"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
-			"taxonomy": "FROM THE TAXONOMY LIST",
-			"tags": ["gas", "can"],
-			"height": 400, "width": 2500,
-		},
-		{
-			"name": "health-fit-watch",
-			"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
-			"taxonomy": "FROM THE TAXONOMY LIST",
-			"tags": ["health", "fit", "watch"],
-			"height": 400, "width": 1500,
-		},
-		{
-			"name": "home-furniture-chest",
-			"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
-			"taxonomy": "FROM THE TAXONOMY LIST",
-			"tags": ["home", "furniture"],
-			"height": 400, "width": 1500,
-		},
-		{
-			"name": "home-house",
-			"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
-			"taxonomy": "FROM THE TAXONOMY LIST",
-			"tags": ["home", "house"],
-			"height": 400, "width": 1500,
-		},
-		{
-			"name": "home-plug",
-			"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
-			"taxonomy": "FROM THE TAXONOMY LIST",
-			"tags": ["home", "plug"],
-			"height": 400, "width": 1500,
-		},
-		{
-			"name": "home-tables-stacked",
-			"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
-			"taxonomy": "FROM THE TAXONOMY LIST",
-			"tags": ["home", "tables"],
-			"height": 400, "width": 1500,
-		},
-		//	{ 
-		//	"name":"hoverboard", "title":"A TITLE IN SENTENCE CASE FOR AWARDS", "taxonomy":"FROM THE TAXONOMY LIST", 
-		//	"tags": ["electronics","hoverboard"], "height":400, "width":1500,  },
-		//	{ 
-		//	"name":"lady-lamp", "title":"A TITLE IN SENTENCE CASE FOR AWARDS", "taxonomy":"FROM THE TAXONOMY LIST", 
-		//	"tags": ["electronics","lamp"], "height":400, "width":2500,  },
-		//	{ 
-		//	"name":"purse", "title":"A TITLE IN SENTENCE CASE FOR AWARDS", "taxonomy":"FROM THE TAXONOMY LIST", 
-		//	"tags": ["apparal","purse"], "height":400, "width":1500, },
-		{
-			"name": "shopping-cart",
-			"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
-			"taxonomy": "FROM THE TAXONOMY LIST",
-			"tags": ["shopping", "cart"],
-			"height": 400, "width": 1500,
-		},
-		{
-			"name": "surveillance-camera",
-			"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
-			"taxonomy": "FROM THE TAXONOMY LIST",
-			"tags": ["electronics", "surveillance", "camera"],
-			"height": 400, "width": 1500,
-		},
-		{
-			"name": "toy-sit-spin",
-			"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
-			"taxonomy": "FROM THE TAXONOMY LIST",
-			"tags": ["toy", "spin"],
-			"height": 400, "width": 1500,
-		},
-		//	{ 
-		//	"name":"vr-fish", "title":"A TITLE IN SENTENCE CASE FOR AWARDS", "taxonomy":"FROM THE TAXONOMY LIST", 
-		//	"tags": ["electronics","vr"], "height":400, "width":1500, },
-	]
+
+// MASTER
+
+var monster_data = {
+
+	"appliances-blender": {
+		"name": "appliances-blender",
+		"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+		"taxonomy": ["Home and Garden", "Home Appliances"],
+		"tags": ["home", "appliance", "blender", "smoothie"],
+		"height": 400, "width": 1500,
+	},
+	"car-suv-luxury": {
+		"name": "car-suv-luxury",
+		"title": "Luxury SUV",
+		"taxonomy": ["Automotive", "Auto Body Styles", "SUV"],
+		"tags": ["auto", "car", "suv", "luxury", "style"],
+		"height": 400, "width": 1500,
+	},
+	"finance-bank-atm": {
+		"name": "finance-bank-atm",
+		"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+		"taxonomy": ["Business and Finance", "Business", "Business Accounting and Finance"],
+		"tags": ["business", "money", "currency", "finance", "bank", "atm"],
+		"height": 400, "width": 1500,
+	},
+	"game-controller": {
+		"name": "game-controller",
+		"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+		"taxonomy": ["Video Gaming","Console Games"],
+		"tags": ["game", "controller", "console"],
+		"height": 400, "width": 1500,
+	},
+	"gas-can": {
+		"name": "gas-can",
+		"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+		"taxonomy": ["Business and Industrial", "Energy", "Oil", "Diesel Fuel"],
+		"tags": ["gas", "can", "business", "industrial", "energy", "oil", "diesel", "fuel"],
+		"height": 400, "width": 2500,
+	},
+	"health-fit-watch": {
+		"name": "health-fit-watch",
+		"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+		"taxonomy": ["Healthy Living", "Fitness and Exercise", "Running and Jogging"],
+		"tags": ["health", "sport", "data", "fit", "watch", "fashion", "accessory"],
+		"height": 400, "width": 1500,
+	},
+	"home-furniture-chest": {
+		"name": "home-furniture-chest",
+		"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+		"taxonomy": ["Home and Garden", "Interior Decorating", "Furniture"],
+		"tags": ["home", "furniture", "interior", "chest", "drawers"],
+		"height": 400, "width": 1500,
+	},
+	"home-house": {
+		"name": "home-house",
+		"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+		"taxonomy": ["Home and Garden", "Home"],
+		"tags": ["home", "house", "dinosaur", "garden"],
+		"height": 400, "width": 1500,
+	},
+	"home-plug": {
+		"name": "home-plug",
+		"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+		"taxonomy": ["Travel", "Travel Accessories"],
+		"tags": ["home", "plug", "travel", "accessory", "convertor"],
+		"height": 400, "width": 1500,
+	},
+	"home-tables-stacked": {
+		"name": "home-tables-stacked",
+		"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+		"taxonomy": ["Home and Garden", "Interior Decorating", "Furniture"],
+		"tags": ["home", "furniture", "interior", "table", "ikea"],
+		"height": 400, "width": 1500,
+	},
+	"home-window": {
+		"name": "home-window",
+		"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+		"taxonomy": ["Home and Garden", "Home Improvement", "Window"],
+		"tags": ["home", "house", "garden", "improve", "window"],
+		"height": 400, "width": 1500,
+	},
+	// "lady-lamp": {
+	// 	"name": "lady-lamp",
+	// 	"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+	// 	"taxonomy": ["health", "fit", "watch"],
+	// 	"tags": ["electronics", "lamp"],
+	// 	"height": 400, "width": 2500,
+	// },
+	// "purse": {
+	// 	"name": "purse",
+	// 	"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+	// 	"taxonomy": ["health", "fit", "watch"],
+	// 	"tags": ["apparal", "purse"],
+	// 	"height": 400, "width": 1500,
+	// },
+	"shopping-cart": {
+		"name": "shopping-cart",
+		"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+		"taxonomy": ["Food and Drink", "Food and Grocery Retailers", "Grocery Stores"],
+		"tags": ["shopping", "cart", "grocery", "food", "drink"],
+		"height": 400, "width": 1500,
+	},
+	"surveillance-camera": {
+		"name": "surveillance-camera",
+		"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+		"taxonomy": ["Home and Garden", "Home Security"],
+		"tags": ["electronics", "surveillance", "camera", "security"],
+		"height": 400, "width": 1500,
+	},
+	"toy-hoverboard": {
+		"name": "toy-hoverboard",
+		"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+		"taxonomy": ["Shopping", "Toys"],
+		"tags": ["toy", "hoverboard"],
+		"height": 400, "width": 1500,
+	},
+	"toy-sit-spin": {
+		"name": "toy-sit-spin",
+		"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+		"taxonomy": ["Shopping", "Toy"],
+		"tags": ["toy", "spin", "retro"],
+		"height": 400, "width": 1500,
+	},
+	"toy-spinner": {
+		"name": "toy-spinner",
+		"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+		"taxonomy": ["Shopping", "Toy"],
+		"tags": ["toy", "spin", "fidget", "spinner"],
+		"height": 400, "width": 1500,
+	},
+	// "vr-fish": {
+	// 	"name": "vr-fish",
+	// 	"title": "A TITLE IN SENTENCE CASE FOR AWARDS",
+	// 	"taxonomy": ["health", "fit", "watch"],
+	// 	"tags": ["electronics", "vr"],
+	// 	"height": 400, "width": 1500,
+	// },
+}
+
+
